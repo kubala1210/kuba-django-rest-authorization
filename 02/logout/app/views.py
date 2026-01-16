d029ccb88233caf91ab5c4cf1de5858a48f224bc
kubala1210 <jakubgromala@wp.pl>
2026-01-16 15:27:26 +0000

    02

---
+++ 02/logout/app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)  # Uzasadnienie poniżej
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+# Create your views here.
